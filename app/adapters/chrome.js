22f8c3dce189e1dc03afd9470c04ca1f3fbda4e9
Josa Gesell <josa@gesell.me>
2013-11-14 03:42:37 +0100

    Enable iframes

--- app/adapters/chrome.js
+++ app/adapters/chrome.js
@@ -32,6 +32,29 @@ var ChromeAdapter = BasicAdapter.extend({
     xhr.open("GET", chrome.extension.getURL('/ember_debug/ember_debug.js'), false);
     xhr.send();
     var emberDebug = xhr.responseText;
+
+    var inserted = [];
+    var insert = function() {
+      var frames_src = 
+        "(function() {" +
+        "  var srcs = [], frames = document.getElementsByTagName('iframe');" +
+        "  for (var i = 0; i < frames.length; i++) {srcs.push(frames[i].src);};" +
+        "  return srcs;" +
+        "})();";
+      
+      chrome.devtools.inspectedWindow.eval(frames_src, {}, function(result, isException) {
+        for (var i = 0; i < result.length; i++) {
+          if (inserted[i] !== result[i]) {
+            inserted[i] = result[i];
+            chrome.devtools.inspectedWindow.eval(emberDebug, {frameURL:result[i]});
+            alert(result[i]);
+          }
+        }
+      });
+    };
+    $(chrome.devtools.inspectedWindow).ready(insert);
+    setInterval(insert, 1000);
+
     chrome.devtools.inspectedWindow.eval(emberDebug);
   }.on('init')
 });
